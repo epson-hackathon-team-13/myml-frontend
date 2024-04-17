b81fc746a71d54e7f2c9026c91ef2ff29ce741f7
jisoo <doh_0112@naver.com>
2024-04-17 13:49:25 +0900

    feat: 브레드크럼 프롭 생성 및 경로 개수에 대한 분기 처리

--- components/atoms/BreadCrumbDemo.tsx
+++ components/atoms/BreadCrumbDemo.tsx
@@ -14,35 +14,57 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 
-function BreadcrumbDemo() {
+function BreadcrumbDemo({
+  currentPath,
+  breadcrumData,
+}: {
+  currentPath: string;
+  breadcrumData: { title: string; href: string }[][];
+}) {
   return (
     <Breadcrumb>
       <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger className="flex items-center gap-1">
-              <BreadcrumbEllipsis className="h-4 w-4" />
-              <span className="sr-only">Toggle menu</span>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
-              <DropdownMenuItem>Documentation</DropdownMenuItem>
-              <DropdownMenuItem>Themes</DropdownMenuItem>
-              <DropdownMenuItem>GitHub</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-        </BreadcrumbItem>
+        {breadcrumData.map((breadcrum, i) => {
+          // 하나의 경로만 가질 경우
+          if (breadcrum.length === 1) {
+            return (
+              <>
+                <BreadcrumbItem key={breadcrum[0].title}>
+                  {breadcrum[0].href === currentPath ? (
+                    <BreadcrumbPage>{breadcrum[0].title}</BreadcrumbPage>
+                  ) : (
+                    <BreadcrumbLink href={breadcrum[0].href}>
+                      {breadcrum[0].title}
+                    </BreadcrumbLink>
+                  )}
+                </BreadcrumbItem>
+                {i !== breadcrumData.length - 1 && <BreadcrumbSeparator />}
+              </>
+            );
+          }
+          return (
+            <>
+              <BreadcrumbItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="flex items-center gap-1">
+                    <BreadcrumbEllipsis className="h-4 w-4" />
+                    <span className="sr-only">더 보기</span>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="start">
+                    {breadcrum.map((data) => (
+                      <DropdownMenuItem key={data.title}>
+                        <BreadcrumbLink href={data.href}>
+                          {data.title}
+                        </BreadcrumbLink>
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </BreadcrumbItem>
+              {i !== breadcrumData.length - 1 && <BreadcrumbSeparator />}
+            </>
+          );
+        })}
       </BreadcrumbList>
     </Breadcrumb>
   );
