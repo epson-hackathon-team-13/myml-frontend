92fea781f0ad6ee7dea5b2b8ee01bec50d212d51
jisoo <doh_0112@naver.com>
2024-04-21 21:14:27 +0900

    feat: 네비게이션 컴포넌트 생성

--- components/organism/NavigationDemo.tsx
+++ components/organism/NavigationDemo.tsx
@@ -42,92 +42,84 @@ const ListItem = React.forwardRef<
 });
 ListItem.displayName = "ListItem";
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
+export type NavigationData = Array<
+  | {
+      key: "MENU";
+      title: string;
+      href?: string;
+      contentClass?: string;
+      content: Array<{
+        title: string;
+        href: string;
+        itemClass?: string;
+      }>;
+    }
+  | {
+      key: "LINK";
+      title: string;
+      href: string;
+      contentClass?: string;
+    }
+>;
 
-function NavigationMenuDemo() {
+function NavigationMenuDemo({
+  navigationData,
+  orientation = "horizontal",
+}: {
+  navigationData: NavigationData;
+  orientation?: "horizontal" | "vertical";
+}) {
   return (
     <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem className="" href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem
-                className=""
-                href="/docs/installation"
-                title="Installation"
-              >
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem
-                className=""
-                href="/docs/primitives/typography"
-                title="Typography"
-              >
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
+      <NavigationMenuList
+        className={cn(
+          orientation === "horizontal" ? "" : "flex-col items-start",
+        )}
+      >
+        {navigationData.map((data) => {
+          if (data.key === "LINK") {
+            return (
+              <NavigationMenuItem key={data.title}>
+                <Link
+                  className={cn(
+                    navigationMenuTriggerStyle(),
+                    data.contentClass,
+                  )}
+                  href={data.href}
+                  passHref
+                >
+                  <NavigationMenuLink>{data.title}</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            );
+          }
+          return (
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>{data.title}</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className={cn("grid w-full gap-3 p-4 ", data.contentClass)}>
+                  {data.content.map((item) => (
+                    <ListItem
+                      className={item.itemClass || ""}
+                      key={item.title}
+                      title={item.title}
+                      href={item.href}
+                    />
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          );
+        })}
+
+        {/* <NavigationMenuItem>
           <NavigationMenuTrigger>Components</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <ul
+              className={cn(
+                "grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ",
+              )}
+            >
               {components.map((component) => (
                 <ListItem
                   className=""
@@ -140,14 +132,12 @@ function NavigationMenuDemo() {
               ))}
             </ul>
           </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
+        </NavigationMenuItem> */}
+        {/* <NavigationMenuItem>
+          <Link className={navigationMenuTriggerStyle()} href="/docs" passHref>
+            <NavigationMenuLink>Documentation</NavigationMenuLink>
           </Link>
-        </NavigationMenuItem>
+        </NavigationMenuItem> */}
       </NavigationMenuList>
     </NavigationMenu>
   );
