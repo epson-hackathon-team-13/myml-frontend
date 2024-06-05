233be4404ae0669fe08b67b648d99a65c644735d
jisoo <doh_0112@naver.com>
2024-06-05 19:29:34 +0900

    chore: 환경 세팅

--- output/output/output/utils/priceUtils.ts
+++
@@ -1,8 +0,0 @@
-// 천 단위로 , 붙이는 함수
-const formattedWithComma = (price: string | number) => {
-  const originalPrice = Number(price);
-  const organizedPrice = originalPrice.toLocaleString();
-  return organizedPrice;
-};
-
-export default formattedWithComma;
