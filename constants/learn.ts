5e7333331a03769c51b7ff306962cc0b06109344
jisoo <doh_0112@naver.com>
2024-06-24 10:26:52 +0900

    feat: 학습 페이지 상수 생성

---
+++ constants/learn.ts
@@ -0,0 +1,11 @@
+import { PageTabType } from "@/components/molecules/page-nav-bar";
+
+export const LEARN_All = "all";
+export const LEARN_GIRL = "girl";
+export const LEARN_BOY = "boy";
+
+export const LEARN_TAB: PageTabType = [
+  { text: "All", value: LEARN_All },
+  { text: "Girl Group", value: LEARN_GIRL },
+  { text: "Boy Group", value: LEARN_BOY },
+];
